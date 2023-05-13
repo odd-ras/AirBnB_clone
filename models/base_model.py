bff287434de00f43236f2c77cdbc894f812078b6
Michael Peter <michaelhpet@gmail.com>
2023-05-13 23:05:32 +0100

    fix: use datetime.now() in place of .today()

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,8 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel object."""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs is None:
             kwargs = {}
@@ -43,7 +43,7 @@ class BaseModel():
 
     def save(self):
         """Update the attribute updated_at with the current datetime."""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
