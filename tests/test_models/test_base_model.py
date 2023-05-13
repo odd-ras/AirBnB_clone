089848d5b1bfeeb7a86f65a92571cf75de037d93
Michael Peter <michaelhpet@gmail.com>
2023-05-13 15:27:29 +0100

    chore: write tests for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,10 +2,94 @@
 """Module for testing BaseModel class."""
 import unittest
 from models.base_model import BaseModel
+from models import storage
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
-    """Define tests for BaseModel."""
+    """Define unit tests for BaseModel."""
 
-    def test_all(self):
-        pass
+    def test_instance(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj, BaseModel)
+
+    def test_instance_with_none(self):
+        obj = BaseModel(None)
+        self.assertNotIn(None, obj.__dict__.values())
+
+    def test_stored_instances(self):
+        objs = storage.all()
+        for key in objs:
+            self.assertIsInstance(objs[key], BaseModel)
+
+    def test_attributes(self):
+        obj = BaseModel()
+        self.assertIsNotNone(obj.id)
+        self.assertIsNotNone(obj.created_at)
+        self.assertIsNotNone(obj.updated_at)
+
+    def test_id_is_str(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.id, str)
+
+    def test_created_at_is_datetime(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        obj = BaseModel()
+        self.assertIsInstance(obj.updated_at, datetime)
+
+    def test_id_is_unique(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertNotEqual(obj1.id, obj2.id)
+
+    def test_serial_created_at(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertNotEqual(obj1.created_at, obj2.created_at)
+        obj1_timestamp = obj1.created_at.timestamp()
+        obj2_timestamp = obj2.created_at.timestamp()
+        self.assertLess(obj1_timestamp, obj2_timestamp)
+
+    def test_serial_updated_at(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertNotEqual(obj1.updated_at, obj2.updated_at)
+        obj1_timestamp = obj1.updated_at.timestamp()
+        obj2_timestamp = obj2.updated_at.timestamp()
+        self.assertLess(obj1_timestamp, obj2_timestamp)
+
+    def test_str(self):
+        obj = BaseModel()
+        obj.id = "ABC"
+        obj_sub_str = "[BaseModel] (ABC)"
+        self.assertIn(obj_sub_str, str(obj))
+
+    def test_to_dict(self):
+        obj = BaseModel()
+        to_dict_result = obj.to_dict()
+        self.assertIsInstance(to_dict_result, dict)
+
+        values = obj.__dict__
+        for key in values:
+            self.assertIn(values[key], to_dict_result.values())
+
+    def test_instance_with_args(self):
+        obj = BaseModel("ABC", "124")
+        self.assertNotEqual("ABC", obj.id)
+        self.assertNotEqual("124", obj.id)
+        self.assertNotIn("ABC", obj.__dict__.values())
+        self.assertNotIn("124", obj.__dict__.values())
+
+    def test_instance_with_kwargs(self):
+        obj = BaseModel(id="ABC")
+        self.assertEqual("ABC", obj.id)
+
+    def test_instance_with_args_kwargs(self):
+        obj = BaseModel("ABC", id="124")
+        self.assertNotEqual("ABC", obj.id)
+        self.assertEqual("124", obj.id)
+        self.assertNotIn("ABC", obj.__dict__.values())
+        self.assertIn("124", obj.__dict__.values())
