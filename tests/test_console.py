8265a827d959b601b4d22da408274ca034a028b1
Michael Peter <michaelhpet@gmail.com>
2023-05-14 14:16:14 +0100

    test: add more tests for cmd methods

--- tests/test_console.py
+++ tests/test_console.py
@@ -87,6 +87,15 @@ class TestHBNBCommand_Execute(unittest.TestCase):
             self.assertNotEqual(output.getvalue().strip(), "\n")
             self.assertGreater(len(output.getvalue().strip()), 0)
 
+    def test_create_method(self):
+        command = "BaseModel.create()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotEqual(output.getvalue().strip(), "\n")
+            self.assertGreater(len(output.getvalue().strip()), 0)
+
     def test_show(self):
         command = "create BaseModel"
         instance_id = ""
@@ -95,6 +104,21 @@ class TestHBNBCommand_Execute(unittest.TestCase):
             instance_id = output.getvalue().strip()
             self.assertIsInstance(instance_id, str)
         command = f"show BaseModel {instance_id}"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotEqual(output.getvalue().strip(), "\n")
+            self.assertIn(instance_id, output.getvalue().strip())
+
+    def test_show_method(self):
+        command = "BaseModel.create()"
+        instance_id = ""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            instance_id = output.getvalue().strip()
+            self.assertIsInstance(instance_id, str)
+        command = f"BaseModel.show({instance_id})"
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd(command)
             self.assertIsInstance(output.getvalue().strip(), str)
@@ -116,6 +140,50 @@ class TestHBNBCommand_Execute(unittest.TestCase):
             HBNBCommand().onecmd(command)
             self.assertEqual("", output.getvalue().strip())
 
+    def test_update_method(self):
+        command = "User.create()"
+        instance_id = ""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            instance_id = output.getvalue().strip()
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotIn("**", output.getvalue().strip())
+        command = f"User.update({instance_id}, first_name, Michael)"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertEqual("", output.getvalue().strip())
+
+    def test_update_method_dict(self):
+        command = "BaseModel.create()"
+        instance_id = ""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            instance_id = output.getvalue().strip()
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotIn("**", output.getvalue().strip())
+        update_dict = {"first_name": "Michael"}
+        command = f"BaseModel.update({instance_id}, {update_dict})"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertEqual("", output.getvalue().strip())
+
+    def test_update_method_dict_multiple(self):
+        command = "User.create()"
+        instance_id = ""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            instance_id = output.getvalue().strip()
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotIn("**", output.getvalue().strip())
+        update_dict = {"first_name": "Michael", "last_name": "Peter"}
+        command = f"User.update({instance_id}, {update_dict})"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertEqual("", output.getvalue().strip())
+
     def test_destroy(self):
         command = "create BaseModel"
         instance_id = ""
@@ -130,6 +198,25 @@ class TestHBNBCommand_Execute(unittest.TestCase):
             HBNBCommand().onecmd(command)
             self.assertEqual("", output.getvalue().strip())
         command = f"show BaseModel {instance_id}"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertEqual("** no instance found **",
+                             output.getvalue().strip())
+
+    def test_destroy_method(self):
+        command = "BaseModel.create()"
+        instance_id = ""
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            instance_id = output.getvalue().strip()
+            self.assertIsInstance(output.getvalue().strip(), str)
+            self.assertNotEqual(output.getvalue().strip(), "")
+            self.assertNotIn("**", output.getvalue().strip())
+        command = f"BaseModel.destroy({instance_id})"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertEqual("", output.getvalue().strip())
+        command = f"BaseModel.show({instance_id})"
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd(command)
             self.assertEqual("** no instance found **",
@@ -154,6 +241,44 @@ class TestHBNBCommand_Execute(unittest.TestCase):
             HBNBCommand().onecmd(command)
             self.assertIn("BaseModel", output.getvalue().strip())
 
+    def test_all_class(self):
+        try:
+            os.remove(objects_path)
+        except FileNotFoundError:
+            pass
+        for _ in range(5):
+            command = "BaseModel.create()"
+            instance_id = ""
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd(command)
+                instance_id = output.getvalue().strip()
+                self.assertIsInstance(output.getvalue().strip(), str)
+                self.assertNotEqual(output.getvalue().strip(), "")
+                self.assertNotEqual(output.getvalue().strip(), "\n")
+        command = "all BaseModel"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertIn("BaseModel", output.getvalue().strip())
+
+    def test_all_class_method(self):
+        try:
+            os.remove(objects_path)
+        except FileNotFoundError:
+            pass
+        for _ in range(5):
+            command = "User.create()"
+            instance_id = ""
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd(command)
+                instance_id = output.getvalue().strip()
+                self.assertIsInstance(output.getvalue().strip(), str)
+                self.assertNotEqual(output.getvalue().strip(), "")
+                self.assertNotEqual(output.getvalue().strip(), "\n")
+        command = "User.all()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertIn("User", output.getvalue().strip())
+
     def test_count(self):
         try:
             os.remove(objects_path)
@@ -172,6 +297,27 @@ class TestHBNBCommand_Execute(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd(command)
             self.assertIsInstance(int(output.getvalue().strip()), int)
+            self.assertGreaterEqual(int(output.getvalue().strip()), 5)
+
+    def test_count_method(self):
+        try:
+            os.remove(objects_path)
+        except FileNotFoundError:
+            pass
+        for _ in range(5):
+            command = "User.create()"
+            instance_id = ""
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd(command)
+                instance_id = output.getvalue().strip()
+                self.assertIsInstance(output.getvalue().strip(), str)
+                self.assertNotEqual(output.getvalue().strip(), "")
+                self.assertNotEqual(output.getvalue().strip(), "\n")
+        command = "User.count()"
+        with patch("sys.stdout", new=StringIO()) as output:
+            HBNBCommand().onecmd(command)
+            self.assertIsInstance(int(output.getvalue().strip()), int)
+            self.assertGreaterEqual(int(output.getvalue().strip()), 5)
 
     def tearDown(self):
         try:
